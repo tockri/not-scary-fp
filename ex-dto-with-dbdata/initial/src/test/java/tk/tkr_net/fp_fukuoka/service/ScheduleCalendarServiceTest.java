67ed4de71798e29f69d39a7dba9f8a324665ab75
Masanori Fujita <fujita@nulab.com>
2020-06-29 01:34:39 +0900

    後始末

--- ex-dto-with-dbdata/initial/src/test/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarServiceTest.java
+++ ex-dto-with-dbdata/initial/src/test/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarServiceTest.java
@@ -21,6 +21,7 @@ public class ScheduleCalendarServiceTest {
 
     @Test
     public void ScheduleCalendarService_makeDto_正常系() {
+        // repositoryにデータを生成
         List.of(
             new Schedule(LocalDate.of(2020, 6, 5), "6月5日-1"),
             new Schedule(LocalDate.of(2020, 6, 30), "6月30日"),
@@ -28,6 +29,7 @@ public class ScheduleCalendarServiceTest {
             new Schedule(LocalDate.of(2020, 6, 5), "6月5日-2")
         ).forEach(scheduleRepository::save);
 
+        // serviceのテスト
         var dto = scheduleCalendarService.makeDto(2020, 6);
         assertEquals(5, dto.getWeeks().size());
         assertEquals(7, dto.getWeeks().get(0).getDays().size());
@@ -35,6 +37,9 @@ public class ScheduleCalendarServiceTest {
         assertEquals("6月5日-1", dto.getWeeks().get(0).getDays().get(5).getSchedules().get(0));
         assertEquals("6月5日-2", dto.getWeeks().get(0).getDays().get(5).getSchedules().get(1));
         assertEquals("6月30日", dto.getWeeks().get(4).getDays().get(2).getSchedules().get(0));
+
+        // repositoryの後始末
+        scheduleRepository.deleteAll();
     }
 
 }
