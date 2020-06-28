cb278d4731f8a43dd800b837ae66e6f74c245aca
Masanori Fujita <fujita@nulab.com>
2020-06-29 01:30:16 +0900

    refactor

--- ex-dto-with-dbdata/complete/src/main/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarDtoBuilder.java
+++ ex-dto-with-dbdata/complete/src/main/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarDtoBuilder.java
@@ -19,21 +19,21 @@ public class ScheduleCalendarDtoBuilder {
         var monthTop = LocalDate.of(year, month, 1);
         // 次月の1日
         var nextMonthTop = monthTop.plusMonths(1);
-        // ループ用変数：月の1日を含む週の日曜日から開始
-        var d = switch (monthTop.getDayOfWeek()) {
+        // 月の1日を含む週の日曜日
+        final var calendarTop = switch (monthTop.getDayOfWeek()) {
             case SUNDAY -> monthTop;
             default -> monthTop.minusDays(monthTop.getDayOfWeek().getValue());
         };
         var weeks = new ArrayList<WeekRowDto>();
-        while (d.isBefore(nextMonthTop)) {
+        for (var sunday = calendarTop; sunday.isBefore(nextMonthTop); sunday = sunday.plusDays(7)) {
             var days = new ArrayList<DayCellDto>();
-            for (var i = 0; i < 7; i++, d = d.plusDays(1)) { // 日曜～土曜の7日間
-                final var date = d;
+            for (var i = 0; i < 7; i++) { // 日曜～土曜の7日間
+                final var date = sunday.plusDays(i);
                 var schedulesInDay = schedules.stream()
                         .filter(s -> s.getDate().equals(date))
                         .map(s -> s.getTitle())
                         .collect(Collectors.toList());
-                days.add(new DayCellDto(d, d.getMonthValue() == month, schedulesInDay));
+                days.add(new DayCellDto(calendarTop, calendarTop.getMonthValue() == month, schedulesInDay));
             }
             weeks.add(new WeekRowDto(days));
         }
--- ex-dto-with-dbdata/initial/src/main/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarService.java
+++ ex-dto-with-dbdata/initial/src/main/java/tk/tkr_net/fp_fukuoka/service/ScheduleCalendarService.java
@@ -21,29 +21,28 @@ public class ScheduleCalendarService {
      * DBからデータを取得してDTOを構築する
      */
     public ScheduleCalendarDto makeDto(int year, int month) {
-        // DBからデータ取得
-        var from = LocalDate.of(year, month, 1);
-        var to = from.plusMonths(1);
-        var schedules = scheduleRepository.findBetween(from, to);
-
         // 月の1日
-        var monthTop = from;
+        var monthTop = LocalDate.of(year, month, 1);
         // 次月の1日
         var nextMonthTop = monthTop.plusMonths(1);
-        // ループ用変数：月の1日を含む週の日曜日から開始
-        var d = switch (monthTop.getDayOfWeek()) {
+        // 月の1日を含む週の日曜日
+        var calendarTop = switch (monthTop.getDayOfWeek()) {
             case SUNDAY -> monthTop;
             default -> monthTop.minusDays(monthTop.getDayOfWeek().getValue());
         };
+        // DBからデータ取得
+        var schedules = scheduleRepository.findBetween(monthTop, nextMonthTop);
+        
         var dto = new ScheduleCalendarDto();
-        while (d.isBefore(nextMonthTop)) {
+        for (var sunday = calendarTop; sunday.isBefore(nextMonthTop); sunday = sunday.plusDays(7)) {
             var weekRow = new WeekRowDto();
-            for (var i = 0; i < 7; i++, d = d.plusDays(1)) { // 日曜～土曜の7日間
-                var dayCell = new DayCellDto(d, d.getMonthValue() == month);
+            for (var i = 0; i < 7; i++) { // 日曜～土曜の7日間
+                var date = sunday.plusDays(i);
+                var dayCell = new DayCellDto(date, date.getMonthValue() == month);
                 weekRow.addDay(dayCell);
                 // スケジュールをセルに設定
                 for (var schedule : schedules) {
-                    if (schedule.getDate().equals(d)) {
+                    if (schedule.getDate().equals(date)) {
                         dayCell.addSchedule(schedule.getTitle());
                     }
                 }
