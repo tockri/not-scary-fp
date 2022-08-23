a4b74e2dd0bab03d84b38b2291897beab73769b0
Masanori Fujita <trickster.mf@gmail.com>
2022-08-24 00:54:07 +0900

    pure fp

--- src/principlistQuickSort.ts
+++ src/principlistQuickSort.ts
@@ -1,22 +1,32 @@
 import { SortAlgorithm } from './SortAlgorithm'
 
-/*
-{ }とreturn文を無くしたい原理主義者のコード
-文が無い = 式の組み合わせだけで表現している = 考え方を最も間違いなく表現している
+/**
+ * Array.concatを関数型っぽい形にするための糖衣
+ */
+const concat = (
+  arr: ReadonlyArray<number>,
+  ...others: ReadonlyArray<number | ReadonlyArray<number>>
+) => arr.concat(...others)
+
+/**
+ * { }とreturn文を無くしたい原理主義者のコード
+ * 文が無い
+ *  = 式の組み合わせだけで表現している
+ *  = 考え方を最も間違いなく表現している
  */
 export const principlistQuickSort: SortAlgorithm = (arr, capture) =>
   arr.length <= 1
     ? arr
     : capture(
-        principlistQuickSort(
-          arr.filter((num) => num < arr[0]),
-          capture
-        )
-          .concat(arr[0])
-          .concat(
-            principlistQuickSort(
-              arr.slice(1).filter((num) => num >= arr[0]),
-              capture
-            )
+        concat(
+          principlistQuickSort(
+            arr.filter((num) => num < arr[0]),
+            capture
+          ),
+          arr[0],
+          principlistQuickSort(
+            arr.slice(1).filter((num) => num >= arr[0]),
+            capture
           )
+        )
       )
